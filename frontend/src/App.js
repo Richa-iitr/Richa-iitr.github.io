2a3007fb29a21c8175f35dec4bad23b235054d1c
Richa-iitr <richa31102@gmail.com>
2022-05-07 19:51:51 +0530

    msg

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import { AppBar } from '@mui/material';
+import './App.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+    </div>
+  );
+}
+
+export default App;
